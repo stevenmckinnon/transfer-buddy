84d160ca9bd1d0611d437ddd901cbfe2b452349d
stevenmckinnon <mckinnon92@gmail.com>
2020-05-10 21:33:19 +0100

    Initial commit

---
+++ build/precache-manifest.3b38a942ead8f23c2d20eb55c643eeac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e68e65f40837c915695609a9fdd83c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c470578886d34224b5ab",
+    "url": "/static/css/main.48117e99.chunk.css"
+  },
+  {
+    "revision": "16d7fae2d607a22f3aad",
+    "url": "/static/js/2.0b65ca64.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0b65ca64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c470578886d34224b5ab",
+    "url": "/static/js/main.ed00f5ce.chunk.js"
+  },
+  {
+    "revision": "a49ba8336855e9354d83",
+    "url": "/static/js/runtime-main.d4e8e9e5.js"
+  },
+  {
+    "revision": "0452b2c3d7877f68a4ebd78769fac4d7",
+    "url": "/static/media/dolphin.0452b2c3.png"
+  },
+  {
+    "revision": "570320ee8cd7155420bb53ce92311ad0",
+    "url": "/static/media/eco.570320ee.png"
+  },
+  {
+    "revision": "20f70d13b0398c70baaa243b3d71124a",
+    "url": "/static/media/example.20f70d13.png"
+  }
+]);
---
+++ src/components/App.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Container, Row, Col, Image, Button, Navbar } from 'react-bootstrap';
+import '../styles/app.scss';
+import * as Example from '../images/example.png';
+import * as Dolphin from '../images/dolphin.png';
+import * as Eco from '../images/eco.png';
+
+const App: React.FC<{}> = () => {
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#">Transfer Buddy®</Navbar.Brand>
+                    </Navbar>
+                </Col>
+            </Row>
+            <Row id="page-container">
+                <Col xs={12}>
+                    <p>
+                        <i>We all need a little bit of luxury in our lives even away from home.</i><br /><br />
+                        The <i>Transfer Buddy®</i> is a simple way of decanting your favourite brands into a handy collapsible pouch. If you love to get away for a camping trip, cycling touring holidays, walking holidays, a picnic, a drive in the country, a weekend away in a hotel, the <i>Transfer Buddy®</i>® allows you to decant any flowable material from moisturiser to sun-cream to your favourite shampoo.<br />
+                        The <i>Transfer Buddy®</i> allows you to buy in bulk and take as little, or as much, as you like with you when you head out on those well-earned breaks as we all emerge from lockdown. It is economical and better for the environment as you can wash out the pouch to use on the next adventure.<br />
+                        As we all start to recover from this unprecedented period the opportunity to treat ourselves, our family and friends with beach days, picnics, weekends, family camping excursions, cycling trips, backpacking into the wilds, getting the motorbike out, whatever your pleasure is, use the <i>Transfer Buddy®</i> to take that treat with you. The joy of never again having to hunt for an old plastic bottle to decant milk into for the tea/coffee, take that last bit in the wine bottle in the fridge. You can transfer hand sanitiser into a pouch via the <i>Transfer Buddy®</i> and have it conveniently with you. The possibilities are endless. <br />
+                        The <i>Transfer Buddy®</i> is designed and made from materials that are safe and reusable. The pouches are robust and collapse so they can be stuffed into picnic hampers (stop things rattling!), rucksacks, cycle bags, panniers, hand luggage on flights (<b>50ml</b> and <b>100ml</b> volume options), once again the possibilities are endless. You can even freeze the pouches with your favourite soups, sauces, gourmet secret recipes, so versatile, easy to use and saves you money. Great if you have a young family as you can mix their favourite drinks, purees and kids can drink directly from the pouches, saving time, money and mess. <br />
+                        Be kind to yourself, your family and friends, use the <i>Transfer Buddy®</i> and take a little luxury with you wherever you go.
+                    </p>
+                    <Image src={String(Example)} fluid />
+                    <h2>Help us develop Transfer Buddy®!</h2>
+                    <p>Help us by taking a few of minutes to complete our survey</p>
+                    <Button variant="primary" href="https://www.surveymonkey.co.uk/r/J2HGM5S">Take Suvery</Button>
+                </Col>
+            </Row>
+            <Row id="footer">
+                <Col xs={12}>
+                    <p className="company-name">Transfer Buddy®, 2020</p>
+                    <div className="images">
+                        <Image src={String(Dolphin)} />
+                        <Image src={String(Eco)} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default App;
